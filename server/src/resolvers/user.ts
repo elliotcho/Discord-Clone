b2bbabcd3da6ebbf948c9ed12724f22746e50044
Yong <ycho97@uwo.ca>
2021-02-17 21:51:43 -0500

    tested graphql

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,15 +1,47 @@
 import { 
     Arg,
     Mutation, 
-    Resolver 
+    Query, 
+    Resolver
 } from "type-graphql";
+import { getConnection } from "typeorm";
+import { User } from "../entities/User";
 
-@Resolver()
+@Resolver(User)
 export class UserResolver {
     @Mutation(() => Boolean)
-    createDummyUser(
+    async createDummyUser(
         @Arg('username') username: string
-    ) : boolean {
-        return username.length === 3;
+    ) : Promise<boolean> {
+        await getConnection().query(
+            `
+                insert into "user" (username) values ('${username}')
+            `
+        );
+
+        return true;
+    }
+
+    @Query(() => [User])
+    async users() : Promise<User[]> {
+        const users = await getConnection().query(
+            `select * from "user"`
+        );
+
+        return users;
+    }
+
+    @Mutation(() => Boolean)
+    async deleteUser(
+        @Arg('username') username: string
+    ) : Promise<boolean> {
+        await getConnection().query(
+            `
+              delete from "user" where username = $1
+            `,
+            [username]
+        );
+
+        return true;
     }
 }
