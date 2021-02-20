757011b10cec439e48a19638dfbc75da098b38a2
Yong <ycho97@uwo.ca>
2021-02-19 23:05:55 -0500

    layout for view teams

---
+++ web/src/components/view-teams/ChatHeader.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChatHeader: React.FC<{}> = () => {
+    return (
+        <h2 style={{background: 'orangered', padding: 0, margin: 0}}>
+            General
+        </h2>
+    )
+}
+
+export default ChatHeader;
---
+++ web/src/components/view-teams/SendMessage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SendMessage: React.FC<{}> = () => {
+    return (
+        <div style={{background: 'yellow', height: '50px'}}>
+
+        </div>
+    )
+}
+
+export default SendMessage;
