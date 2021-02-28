1032210c80236311f0c22f9e49dce93170ebaa28
Armaan Mohar <armaan.mohar@gmail.com>
2021-02-27 20:19:53 -0700

    message

---
+++ server/src/entities/Message.ts
@@ -0,0 +1,39 @@
+import {Field, ObjectType} from "type-graphql";
+import{
+    BaseEntity,
+    Column,
+    CreateDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn
+} from "typeorm";
+
+@ObjectType()
+@Entity()
+export class Message extends BaseEntity{
+    @Field()
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Field()
+    @Column()
+    message: string;
+
+    @Field()
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @Field()
+    @UpdateDateColumn()
+    edited: Date;
+
+    @Field()
+    @Column()
+    senderID: number;
+
+    @Field()
+    @Column()
+    channelID: number;
+
+
+}
---
+++ server/src/resolvers/message.ts
@@ -0,0 +1,46 @@
+import {
+    Arg,
+    Ctx,
+    Mutation,
+    Query,
+    Resolver,
+} from "type-graphql";
+import {MyContext} from "../types";
+import {GraphQLUpload} from 'graphql-upload';
+import {v4} from 'uuid';
+import {getConnection} from "typeorm";
+import {Message} from "../entities/Message";
+
+@Resolver(Message)
+export class MessageResolver {
+    @Mutation(() => Boolean)
+    async sendMessage(
+        @Arg('message') message: string,
+        @Arg('channelID') channelID: number,
+        @Ctx() { req } : MyContext 
+    ): Promise<boolean>{
+        const senderID = req.session.uid;
+        await getConnection().query(
+            `
+             insert into "message" (message, senderID, channelID)
+             values($1,$2,$3)
+            `, 
+            [message, senderID, channelID]
+        );
+        return true;
+
+    }
+
+    @Query(() => [Message])
+    async getMessages(
+        @Arg('channelID') channelID: number,
+    ): Promise<Message | undefined> {
+        const messages = await getConnection().query(
+            `
+            select (message,createdAt, edited) from "message"
+            where channelID=$1
+            `, [channelID]
+        );
+        return messages;
+    }
+}
