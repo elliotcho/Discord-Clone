046f7d188f6dbc077ac12b6e2198189b8838b56d
Charan Gautam <gautamsaicharan@yahoo.com>
2021-03-08 20:56:17 -0700

    profile

--- web/src/containers/profile/ProfileContainer.tsx
+++ web/src/containers/profile/ProfileContainer.tsx
@@ -1,6 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
 import { useMeQuery } from '../../generated/graphql';
 import { isServer } from '../../utils/isServer';
+
+const Container = styled.div`
+    background: #222831;
+    color: #e6e6e6;
+    padding: 3%;
+`;
+
+const Edit = styled.div`
+    width: 50%;
+    background: #0f1123;
+    margin: 91px 20px;
+    padding: 1%;
+    border-radius: 50px;
+`;
+
+const Form = styled.form`
+    margin: 20px;
+`;
+
+const Label = styled.label`
+    text-align: left;
+    margin: 0 30px 10px 0;
+`;
+
+const Button = styled.button`
+    background: #a6f0c6;
+    text-align: center;
+    border-radius: 20px;
+    border: solid;
+`;
+
+
 
 const ProfileContainer: React.FC<{}> = () => {
     const { data } = useMeQuery();
@@ -8,14 +41,31 @@ const ProfileContainer: React.FC<{}> = () => {
     let username = data?.me?.username || 'Loading...';
 
     return (
-        <div>
-       <h2> Whagwan, {username} </h2>
-       <form>
-            <label htmlFor="myFile">Update Profile Picture:</label>
-            <input type="file" id="myFile" name="myFile"></input>
-       </form>
+        <Container>
+            <h2> Whagwan, {username} </h2>
+            
+            <Edit>
+                <Form>
+                    <Label htmlFor="myFile">update profile picture</Label>
+                    <input type="file" id="myFile" name="myFile" />
+                </Form>
+               
+                <Form>
+                    <Label htmlFor="usernane">change username</Label>
+                    <input type='username' id='username' name='username' />
+                    <Button type='submit'>✔️</Button>
+                </Form>
+
+                <Form>
+                    <Label htmlFor='email'>change email</Label>
+                    <input type='email' id='email' name='email' />
+                    <Button type='submit'>✔️</Button>
+                </Form>
+                
+            </Edit>
+
         
-        </div>
+        </Container>
     )
 }
 
